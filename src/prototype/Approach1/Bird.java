ae1cf8d97e74caf00e6e68f6003730684160d573
mkharshita <mkharshita5@gmail.com>
2024-06-05 22:53:19 +0530

    prototype DP code

---
+++ src/prototype/Approach1/Bird.java
@@ -0,0 +1,34 @@
+package prototype.Approach1;
+
+public class Bird implements Cloneable<Bird> {
+
+    private String name;
+    private String color;
+    private int weight;
+
+    public Bird() {
+    }
+
+    public Bird(Bird old) {
+        this.name = old.name;
+        this.color = old.color;
+        this.weight = old.weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public Bird clone() {
+        return new Bird(this);
+    }
+}
---
+++ src/prototype/Approach1/Cloneable.java
@@ -0,0 +1,7 @@
+package prototype.Approach1;
+
+import java.util.ArrayList;
+
+public interface Cloneable<E> {
+    E clone();
+}
